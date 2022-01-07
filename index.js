33ab2492694c89b997b65764e89dd077e5be4911
M4tsuti <ctsinon@outlook.com>
2022-01-07 15:28:01 +0800

    added support for decrypt

--- index.js
+++ index.js
@@ -12,9 +12,41 @@ async function decrypt(cipher, key) {
 }
 
 export async function encryptInput() {
-    const msg = document.getElementById("plain").value;
+    const file = document.getElementById("plain").value;
     const key = document.getElementById("enc_key").value;
-    const res = await encrypt(msg, key);
-    console.log(res)
-    document.getElementById("enc_res").innerHTML = res;
+
+    await processInput(file, key, encrypt, file + ".enc");
+}
+
+export async function decryptInput() {
+    const file = document.getElementById("cipher").files[0];
+    const key = document.getElementById("dec_key").value;
+
+    await processInput(file, key, decrypt, file.toString() + ".dec");
+}
+
+async function processInput(file, key, processor, out_name) {
+
+    // 使用FileReader读取文件
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    // 读取成功时，将文件内容转为Uint8Array，传递给加密函数
+    reader.onload = async content => {
+        const plain = new Uint8Array(content.target.result);
+
+        const res = await processor(plain, key);
+
+        // 下载文件
+        const blob = new Blob([res], { type: "application/octet-stream" });
+        const link = document.createElement('a');
+        link.href = window.URL.createObjectURL(blob);
+        link.download = out_name;
+        link.click();
+    }
+
+    // 失败时在console打log
+    reader.onerror = _ => {
+        console.log("Error occurred when reading file content.")
+    }
 }
