4182455fed82a38241862d2b1b4bc4bdc2af40f4
Autosaida <58523110+Autosaida@users.noreply.github.com>
2022-01-08 17:27:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const sm4_pkg =
     import ("./sm4_backend/pkg/sm4_backend_bg.js");
 
+var fileSize = 0;
+var costTime = 0;
 /**
  * 使用String key加密一个Uint8Array
  * @param {Uint8Array} msg 要加密的内容
@@ -33,15 +35,15 @@ async function decrypt(cipher, key) {
 }
 
 export async function encryptInput() {
-    const file = document.getElementById("plain").files[0];
-    const key = document.getElementById("enc_key").value;
+    const file = document.getElementById("selected_file").files[0];
+    const key = document.getElementById("key").value;
 
     await processInput(file, key, encrypt, file.name + ".enc");
 }
 
 export async function decryptInput() {
-    const file = document.getElementById("cipher").files[0];
-    const key = document.getElementById("dec_key").value;
+    const file = document.getElementById("selected_file").files[0];
+    const key = document.getElementById("key").value;
 
     await processInput(file, key, decrypt, file.name + ".dec");
 }
@@ -52,13 +54,16 @@ async function processInput(file, key, processor, out_name) {
     const reader = new FileReader();
     reader.readAsArrayBuffer(file);
 
+    document.getElementById('progress_percent').innerHTML = '';
+    var oProgress = document.getElementById('progress');
+    oProgress.style.display = 'block';
+    oProgress.style.width = '0px';
+
     // 读取成功时，将文件内容转为Uint8Array，传递给加密函数
     reader.onload = async content => {
         const plain = new Uint8Array(content.target.result);
 
         const [time, res] = await processor(plain, key);
-
-        console.log(`Process took ${time} ms`);
 
         // 下载文件
         const blob = new Blob([res], { type: "application/octet-stream" });
@@ -66,10 +71,51 @@ async function processInput(file, key, processor, out_name) {
         link.href = window.URL.createObjectURL(blob);
         link.download = out_name;
         link.click();
+
+        
+        costTime = time / 1000;
+        document.getElementById('progress').style.width = (650).toString() + 'px';
+        document.getElementById('progress_percent').innerHTML =  "100%";
+        document.getElementById('cost').innerHTML = '| ' + secondsToTime(costTime);            
+        document.getElementById('speed').innerHTML = '| ' + (fileSize/costTime).toFixed(2) + ' bytes/s';        
     }
 
     // 失败时在console打log
     reader.onerror = _ => {
         console.log("Error occurred when reading file content.")
     }
+}
+
+function secondsToTime(secs) { // we will use this function to convert seconds in normal time format
+    var hr = Math.floor(secs / 3600);
+    var min = Math.floor((secs - (hr * 3600))/60);
+    var sec = Math.floor(secs - (hr * 3600) -  (min * 60));
+
+    if (hr < 10) {hr = "0" + hr; }
+    if (min < 10) {min = "0" + min;}
+    if (sec < 10) {sec = "0" + sec;}
+    if (hr) {hr = "00";}
+    return hr + ':' + min + ':' + sec;
+};
+
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB'];
+    if (bytes == 0) return 'n/a';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+};
+
+export function fileSelected() {
+    var oFile = document.getElementById('selected_file').files[0];
+
+    var oReader = new FileReader();
+    oReader.onload = function(e) {
+        oFile.src = e.target.result;
+        document.getElementById('fileinfo').style.display = 'block';
+        document.getElementById('filename').innerHTML = 'Name: ' + oFile.name;
+        document.getElementById('filesize').innerHTML = 'Size: ' + bytesToSize(oFile.size);
+        document.getElementById('filetype').innerHTML = 'Type: ' + oFile.type;    
+        fileSize = oFile.size;
+    };
+    oReader.readAsDataURL(oFile);
 }
