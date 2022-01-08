223849fcac390dc309ecb364a7591a2854d8b74c
M4tsuti <ctsinon@outlook.com>
2022-01-08 19:38:06 +0800

    frontend file structure changed

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: "./index.js",
     output: {
-        path: path.resolve(__dirname, "sm4_server/public"),
+        path: path.resolve(__dirname, "sm4_server/public/dist"),
         filename: "index.js",
         library: "index",
         libraryTarget: 'window',
